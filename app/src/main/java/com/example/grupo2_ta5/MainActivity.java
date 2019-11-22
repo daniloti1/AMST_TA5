daa3f13361ddc566d8f59bed517103a1d851d209
daniloti1 <danilo.ti@hotmail.com>
2019-11-22 12:26:18 -0500

    Avance

--- app/src/main/java/com/example/grupo2_ta5/MainActivity.java
+++ app/src/main/java/com/example/grupo2_ta5/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.grupo2_ta5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean VerificarMesa(int datos) {
+        boolean bandera = true;
+        if (datos == 0) {
+            bandera = false;
+        }
+        return bandera;
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            HashMap<String, String> info_user = new HashMap<String, String>();
+            info_user.put("user_name", user.getDisplayName());
+            info_user.put("user_email", user.getEmail());
+            info_user.put("user_photo", String.valueOf(user.getPhotoUrl()));
+            info_user.put("user_id", user.getUid());
+            info_user.put("user_provider_id", user.getProviderId());
+            if (user.getPhoneNumber() != null){
+                info_user.put("user_phone_number", user.getPhoneNumber());
+            }else{
+                info_user.put("user_phone_number", "Sin Numero");
+            }
+
+            finish();
+            /*
+            Intent intent = new Intent(this, PerfilUsuario.class);
+            intent.putExtra("info_user", info_user);
+            startActivity(intent);
+             */
+        } else {
+            System.out.println("Sin Registrarse");
+        }
     }
 }
